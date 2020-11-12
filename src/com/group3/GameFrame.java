27bcc36022f4a79d49d6fb2f966f5b3eb140994d
Nathan Bianco <nathan.bianco@tamu.edu>
2020-11-11 22:01:07 -0600

    Complete graphics

--- src/com/group3/GameFrame.java
+++ src/com/group3/GameFrame.java
@@ -81,55 +81,117 @@ public class GameFrame extends JFrame {
 
         public void paint(Graphics g) {
             Graphics2D g2d = (Graphics2D) g;
+
             // Draw Background
             g2d.setColor(Color.WHITE);
             g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
-
             g2d.setColor(Color.BLACK);
+
             // Draw Player 1 Info
-            int x = 0, y = 0;
-
-
-            // Draw Player 2 Info
-            x = 0; y = 0;
+            int x = 0, y = FRAME_HEIGHT - Y_PAD*4;
             g2d.setFont(BIG_FONT);
-            g2d.drawString(player2Name, x += X_PAD, y += (int)(Y_PAD*3/2));
+            g2d.drawString(player1Name, x += X_PAD, y - (int)(Y_PAD*2.0));
+            y -= CARD_HEIGHT + (int)(Y_PAD*1.1);
             g2d.setFont(SMALL_FONT);
-            // if (hand.length() > 0) {
-                g2d.drawImage(getImage("back.png"), x += X_PAD*6, y, this);
+            // if (hand1.length() > 0) {
+                g2d.drawImage(getImage("back.png"), x += X_PAD*12, y, this);
                 g2d.drawRect(x, y, CARD_WIDTH, CARD_HEIGHT);
-                g2d.drawString("Hand", x + CARD_WIDTH - (int)(X_PAD*4.5), y - (int)(Y_PAD/2));
+                g2d.drawString("Hand", x + CARD_WIDTH - (int)(X_PAD*4.5), y + Y_PAD + CARD_HEIGHT);
             // }
-            // if (hand.length() > 1) {
+            // if (hand1.length() > 1) {
                 g2d.drawImage(getImage("back.png"), x + X_PAD, y, this);
                 g2d.drawRect(x + X_PAD, y, CARD_WIDTH, CARD_HEIGHT);
             // }
-            // if (hand.length() > 2) {
+            // if (hand1.length() > 2) {
                 g2d.drawImage(getImage("back.png"), x + X_PAD*2, y, this);
                 g2d.drawRect(x + X_PAD*2, y, CARD_WIDTH, CARD_HEIGHT);
             // }
-            // if (flipCard != null) {
-                g2d.drawImage(getImage("hearts_7.png"), x += CARD_WIDTH + X_PAD*4, y, this);
+            x += CARD_WIDTH + X_PAD*3;
+            // if (flipCard1 != null) {
+                g2d.drawImage(getImage("spades_10.png"), x, y, this);
                 g2d.drawRect(x, y, CARD_WIDTH, CARD_HEIGHT);
-                g2d.drawString("Flip Card", x + X_PAD*3, y - (int)(Y_PAD/2));
+                g2d.drawString("Flip Card", x + X_PAD*3, y + Y_PAD + CARD_HEIGHT);
             // }
-            // if (warTopCard != null) {
-                g2d.drawImage(getImage("clubs_K.png"), x += CARD_WIDTH + X_PAD*2, y, this);
+            x += CARD_WIDTH + X_PAD*4;
+            // if (warCards1.length() > 2) {
+                g2d.drawImage(getImage("back.png"), x, y - Y_PAD*2, this);
+                g2d.drawRect(x, y - Y_PAD*2, CARD_WIDTH, CARD_HEIGHT);
+            // }
+            // if (warCards1.length() > 1) {
+                g2d.drawImage(getImage("back.png"), x, y - Y_PAD, this);
+                g2d.drawRect(x, y - Y_PAD, CARD_WIDTH, CARD_HEIGHT);
+            // }
+            // if (warCards1.length() > 0) {
+                g2d.drawImage(getImage("hearts_Q.png"), x, y, this);
                 g2d.drawRect(x, y, CARD_WIDTH, CARD_HEIGHT);
-                g2d.drawString("War", x + X_PAD*3, y - (int)(Y_PAD/2));
+                g2d.drawString("War", x + X_PAD*3, y + Y_PAD + CARD_HEIGHT);
             // }
-            // if (hand.length() > 0) {
-                g2d.drawImage(getImage("diamonds_A.png"), x += CARD_WIDTH + X_PAD*2, y, this);
+            x += CARD_WIDTH + X_PAD*4;
+            // if (winPile1.length() > 0) {
+                g2d.drawImage(getImage("spades_A.png"), x, y, this);
                 g2d.drawRect(x, y, CARD_WIDTH, CARD_HEIGHT);
-                g2d.drawString("Win Pile", x + CARD_WIDTH - X_PAD*5, y - (int)(Y_PAD/2));
+                g2d.drawString("Win Pile", x + CARD_WIDTH - X_PAD*5, y + Y_PAD + CARD_HEIGHT);
             // }
-            // if (hand.length() > 1) {
-                g2d.drawImage(getImage("clubs_2.png"), x + X_PAD, y, this);
+            // if (winPile1.length() > 1) {
+                g2d.drawImage(getImage("clubs_10.png"), x + X_PAD*2, y, this);
+                g2d.drawRect(x + X_PAD*2, y, CARD_WIDTH, CARD_HEIGHT);
+            // }
+            // if (winPile1.length() > 2) {
+                g2d.drawImage(getImage("diamonds_2.png"), x + X_PAD*4, y, this);
+                g2d.drawRect(x + X_PAD*4, y, CARD_WIDTH, CARD_HEIGHT);
+            // }
+
+            // Draw Player 2 Info
+            x = 0; y = Y_PAD;
+            g2d.setFont(BIG_FONT);
+            g2d.drawString(player2Name, x += X_PAD, (y += (int)(Y_PAD*1.5)) + (int)(Y_PAD*1.8));
+            g2d.setFont(SMALL_FONT);
+            // if (hand2.length() > 0) {
+                g2d.drawImage(getImage("back.png"), x += X_PAD*12, y, this);
+                g2d.drawRect(x, y, CARD_WIDTH, CARD_HEIGHT);
+                g2d.drawString("Hand", x + CARD_WIDTH - (int)(X_PAD*4.5), y - (int)(Y_PAD*0.5));
+            // }
+            // if (hand2.length() > 1) {
+                g2d.drawImage(getImage("back.png"), x + X_PAD, y, this);
                 g2d.drawRect(x + X_PAD, y, CARD_WIDTH, CARD_HEIGHT);
             // }
-            // if (hand.length() > 2) {
-                g2d.drawImage(getImage("hearts_4.png"), x + X_PAD*2, y, this);
+            // if (hand2.length() > 2) {
+                g2d.drawImage(getImage("back.png"), x + X_PAD*2, y, this);
                 g2d.drawRect(x + X_PAD*2, y, CARD_WIDTH, CARD_HEIGHT);
+            // }
+            x += CARD_WIDTH + X_PAD*3;
+            // if (flipCard2 != null) {
+                g2d.drawImage(getImage("hearts_7.png"), x, y, this);
+                g2d.drawRect(x, y, CARD_WIDTH, CARD_HEIGHT);
+                g2d.drawString("Flip Card", x + X_PAD*3, y - (int)(Y_PAD*0.5));
+            // }
+            x += CARD_WIDTH + X_PAD*4;
+            // if (warCards2.length() > 2) {
+                g2d.drawImage(getImage("back.png"), x, y + Y_PAD*2, this);
+                g2d.drawRect(x, y + Y_PAD*2, CARD_WIDTH, CARD_HEIGHT);
+            // }
+            // if (warCards2.length() > 1) {
+                g2d.drawImage(getImage("back.png"), x, y + Y_PAD, this);
+                g2d.drawRect(x, y + Y_PAD, CARD_WIDTH, CARD_HEIGHT);
+            // }
+            // if (warCards2.length() > 0) {
+                g2d.drawImage(getImage("clubs_K.png"), x, y, this);
+                g2d.drawRect(x, y, CARD_WIDTH, CARD_HEIGHT);
+                g2d.drawString("War", x + X_PAD*3, y - (int)(Y_PAD*0.5));
+            // }
+            x += CARD_WIDTH + X_PAD*4;
+            // if (winPile2.length() > 0) {
+                g2d.drawImage(getImage("diamonds_A.png"), x, y, this);
+                g2d.drawRect(x, y, CARD_WIDTH, CARD_HEIGHT);
+                g2d.drawString("Win Pile", x + CARD_WIDTH - X_PAD*5, y - (int)(Y_PAD*0.5));
+            // }
+            // if (winPile2.length() > 1) {
+                g2d.drawImage(getImage("clubs_2.png"), x + X_PAD*2, y, this);
+                g2d.drawRect(x + X_PAD*2, y, CARD_WIDTH, CARD_HEIGHT);
+            // }
+            // if (winPile2.length() > 2) {
+                g2d.drawImage(getImage("hearts_4.png"), x + X_PAD*4, y, this);
+                g2d.drawRect(x + X_PAD*4, y, CARD_WIDTH, CARD_HEIGHT);
             // }
         }
 
